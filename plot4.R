2c8980baaaa65228039c3e33203a6d103cedc8b9
Antonio Adsuar <antonio.adsuar.gomez@gmail.com>
2015-05-08 01:57:22 +0200

    iSolution to Plot4 exercise.

---
+++ plot4.R
@@ -0,0 +1,44 @@
+#########################################################
+#
+# filename: plot4.R
+# author: Antonio Adsuar
+# description: script that generates the fourth plot of
+#              the first course project of the Coursera's
+#              Exploratory Data Analysis course.
+
+# Removal of previousily created variables
+rm(list=ls())
+
+# We read the data file, using as a separator the ';' character,
+# avoiding the creation of factors, and assigning NA values to
+# cells valued as '?'
+hpc <- read.csv("household_power_consumption.txt",header=T,sep=";",stringsAsFactors=F, na.strings="?")
+
+# Remove the lines that are different from February first and second, 2007.
+# I don't need to perform any transformation over the data.
+hpc_filtered <- hpc[hpc[,"Date"] == "1/2/2007" | hpc[,"Date"] == "2/2/2007",]
+
+# Now I need to create a custom column to show in the x-axis the
+# results between Thursday and Saturday.
+# Thanks to the "Discussion Forums" :D
+datetime <- strptime(paste(hpc_filtered$Date, hpc_filtered$Time), format="%d/%m/%Y %H:%M:%S", tz="C")
+hpc_filtered$DateTime <- datetime
+
+# Graphical device; we don't need to specify height and width to 480
+# since the default value is 480 in both cases.
+png("plot4.png",width=480,height=480)
+# We create a 2x2 grid for plotting the results
+par(mfrow=c(2,2))
+# We plot the Active Power.
+plot(x=hpc_filtered$DateTime, y=hpc_filtered[,"Global_active_power"],type="l",xlab="", ylab="Global Active Power")
+# We plot the Voltage.
+plot(x=hpc_filtered$DateTime, y=hpc_filtered[,"Voltage"],type="l",xlab="datetime", ylab="Voltage")
+# We plot the third graph.
+plot(x=hpc_filtered$DateTime, y=hpc_filtered[,"Sub_metering_1"],type="l",xlab="", ylab="Energy sub metering", col="black")
+lines(x=hpc_filtered$DateTime, y=hpc_filtered[,"Sub_metering_2"], col="red")
+lines(x=hpc_filtered$DateTime, y=hpc_filtered[,"Sub_metering_3"], col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lwd=2,lty=1)
+# We plot the Reactive Power.
+plot(x=hpc_filtered$DateTime, y=hpc_filtered[,"Global_reactive_power"],type="l",xlab="datetime", ylab="Global_reactive_power")
+# We shutdown the graphical device.
+dev.off()
